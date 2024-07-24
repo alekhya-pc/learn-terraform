ef5876a57b65f4752d77d505b7dc9643e93655bc
Naren <alekhyabonkuri96@gmail.com>
2024-07-24 12:08:42 -0400

    terraform code

---
+++ roboshop-v1/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0f4639e18c6d65317"]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "mongo" {
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0f4639e18c6d65317"]
+
+  tags = {
+    Name = "mongo"
+  }
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0f4639e18c6d65317"]
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+
