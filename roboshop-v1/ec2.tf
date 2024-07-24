64907a0b1233cb178758fdd57805b4c4032d4823
Naren <alekhyabonkuri96@gmail.com>
2024-07-24 12:35:19 -0400

    terraform code

--- roboshop-v1/ec2.tf
+++ roboshop-v1/ec2.tf
@@ -8,6 +8,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = Z02513163LWSSCN6AOOBU
+  name    = "frontend.dev.alekhyab96.online"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongo" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t3.small"
