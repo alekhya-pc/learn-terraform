b1b41bb1eac2071400edcdee9de4522ac3ea79bb
Naren <alekhyabonkuri96@gmail.com>
2024-07-25 21:43:33 -0400

    terraform code

--- roboshop-v2/variables.tf
+++ roboshop-v2/variables.tf
@@ -1,18 +1,6 @@
-variable "ami" {
-    default = "ami-041e2ea9402c46c32"
- }
-
 variable "instance_type" {
     default = "t3.small"
  }
-
-variable "security_group_ids" {
-     default = ["sg-0f4639e18c6d65317"]
-  }
-
-variable "zone_id" {
-     default = "Z02513163LWSSCN6AOOBU"
-  }
 
 variable "domain_name" {
      default = "alekhyab96.online"
