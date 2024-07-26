11f582617741adffb677b3b8130fa12aa90d76d7
Naren <alekhyabonkuri96@gmail.com>
2024-07-25 23:15:31 -0400

    terraform code

--- loops-1/main.tf
+++ loops-1/main.tf
@@ -1,3 +1,11 @@
+#here we are just creating a null resource and trying to iterate it for 3 times
 resource "null_resource" "test" {
     count = 3
+    }
+
+variable "components" {
+    default = ["frontend" , "catalogue" , "mongo"]
+    }
+resource "null_resource" "test1" {
+    count = length(var.components)
     }
