69ee7dacd64e8e12a6f37173579de2fc3bb40b09
Naren <alekhyabonkuri96@gmail.com>
2024-07-26 15:17:44 -0400

    terraform code

--- roboshop-v4/main.tf
+++ roboshop-v4/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "instance" {
  resource "aws_route53_record" "dns_record" {
      for_each = var.components
  zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "${each.key}.${var.domain_name}"
+  name    = "${each.key}.dev.${var.domain_name}"
   type    = "A"
   ttl     = 15
   records = [aws_instance.instance[each.key].private_ip]
