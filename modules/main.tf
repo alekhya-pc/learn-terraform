eae58a4728278ffe213ce3e3fff5c91442fb7db0
Naren <alekhyabonkuri96@gmail.com>
2024-07-29 13:28:42 -0400

    terraform code

---
+++ modules/main.tf
@@ -0,0 +1,38 @@
+# modules are containers for multiple resources that are used together. A module consists of a collection of .tf and/or .tf.json files kept together in a directory.
+
+module "example" {
+    source = "./example"
+    input = 3
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
